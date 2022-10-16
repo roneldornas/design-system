08dbfd6dc9b6ec8f176ad0a625bef3728e1b4306
dornasronel <roneldornas@gmail.com>
2022-10-16 10:32:56 -0300

    first commit

---
+++ src/componets/Checkbox.stories.tsx
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Checkbox, CheckboxProps } from "./Checkbox";
+import { Text } from './Text';
+
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  args: {},
+  argTypes: {},
+  decorators: [
+    (Story) => {
+        return (
+            <div className='flex items-center gap-2'>
+                {Story()}
+
+                <Text size='sm'>Remember me</Text>
+            </div>
+        )
+    }
+  ],
+
+} as Meta<CheckboxProps>
+
+export const Default: StoryObj<CheckboxProps> = {};
